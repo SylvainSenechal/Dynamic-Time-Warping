3bd9eccd87d6e9f6287e99afdc3be4ae61696338
Sylvain Sénéchal <senechalsylvain@hotmail.com>
2019-05-28 05:58:15 +0200

    bug fix & refactor

--- script.js
+++ script.js
@@ -5,118 +5,93 @@ var ctx, canvas
 const initCanvas = () => {
   canvas = document.getElementById('gameCanvas')
   ctx = canvas.getContext('2d')
-
-  let sizeCanvas = document.getElementsByClassName('game')
-
   ctx.canvas.width = window.innerWidth
   ctx.canvas.height = window.innerHeight
 
   canvas.addEventListener('mousedown', startDraw)
   canvas.addEventListener('mouseup', stopDraw)
   canvas.addEventListener('mousemove', draw)
-
 }
 
 let drawing = false
-let distanceBetweenPoints = 50
+let distanceBetweenPoints = 20
 let mouse = {x: 0, y: 0}
 
 const computeMatrix = (sequence1, sequence2) => {
-  console.log(sequence1)
-  let matrix = Array(sequence1.length + 1).fill().map( () => Array(sequence2.length + 1).fill(0))
-  for (let i = 0; i < sequence1.length; i++) { // Ajout sequence 1
-    matrix[sequence1.length - 1 - i][0] = sequence1[i].y
+  let matrix = Array(sequence1.length).fill().map( () => Array(sequence2.length).fill(10000))
+  matrix[0][0] = Math.abs(sequence1[0].y - sequence2[0].y)
+  for (let i = 1; i < sequence1.length; i++) {
+    let cost = Math.abs(sequence1[i].y - sequence2[0].y)
+    matrix[i][0] = cost + matrix[i-1][0]
   }
-  for (let i = 0; i < sequence2.length; i++) { // Ajout sequence 2
-    matrix[sequence1.length][i+1] = sequence2[i].y
-  }
-  matrix[sequence1.length-1][1] = Math.abs(matrix[sequence1.length-1][0] - matrix[sequence1.length][1]) // Ajout angle en bas départ
-  for (let i = 1; i < sequence1.length; i++) { // Ajout premiere colonne
-    matrix[sequence1.length-1-i][1] = Math.abs(matrix[sequence1.length-1-i][0] - matrix[sequence1.length][1]) + matrix[sequence1.length-i][1]
-  }
-  for (let i = 1; i < sequence2.length; i++) { // Ajout derniere ligne
-    matrix[sequence1.length-1][i+1] = Math.abs(matrix[sequence1.length-1][0] - matrix[sequence1.length][1+i]) + matrix[sequence1.length-1][i]
-  }
-  console.table(matrix)
-
-  // Full remplissage :
   for (let i = 1; i < sequence2.length; i++) {
-    for (let j = 1; j < sequence1.length; j++) {
-      let abs = Math.abs(matrix[sequence1.length-1-j][0] - matrix[sequence1.length][1+i])
-      matrix[sequence1.length-1-j][i+1] = abs + Math.min( matrix[sequence1.length-1-j][i], matrix[sequence1.length-j][i], matrix[sequence1.length-j][i+1] )
+    let cost = Math.abs(sequence1[0].y - sequence2[i].y)
+    matrix[0][i] = cost + matrix[0][i-1]
+  }
+  for (let i = 1; i < sequence1.length; i++) {
+    for (let j = 1; j < sequence2.length; j++) {
+      let cost = Math.abs(sequence1[i].y - sequence2[j].y)
+      matrix[i][j] = cost + Math.min( matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1])
     }
   }
-  console.table(matrix)
-
+  // console.table(matrix)
   return matrix
 }
 
-const getOptimalPath = matrix => {
-  let y = 0 // matrix.length - 1 // vertical
-  let x = matrix[0].length - 1 // horizontal
-  // let sequence = [{x: matrix[matrix.length-1][x], y: matrix[0][0]}] // Initialisation en haut a droite matrice
-  let sequence = [{x: x, y: matrix.length-1 - y}] // Initialisation en haut a droite matrice
+const getOptimalPath = (matrix, sequence1, sequence2) => {
+  let bestPath = [{s1: sequence1.length-1, s2: sequence2.length-1}]
+  let i = sequence1.length-1
+  let j = sequence2.length-1
+  while (i > 0 || j > 0) {
+    // let min = Math.min( matrix[i-1][j] || 10000, matrix[i][j-1] || 10000, matrix[i-1][j-1] || 10000)
 
-  while (y != matrix.length - 1 && x != 0) {
-    // console.log("oui")
     let min
-    if ( x-1 === 0) { // Sur la bordure à gauche
-      min = matrix[y+1][x-1]
+    if (i === 0) { // Sur la bordure haute
+      min = matrix[i][j-1]
     }
-    else if ( y-1 === matrix.length ) { // Sur la bordure en bas
-      min = matrix[y+1][x]
+    else if (j === 0) { // Sur la bordure gauche
+      min = matrix[i-1][j]
     }
     else { // Classique
-      min = Math.min(matrix[y][x-1], matrix[y+1][x], matrix[y+1][x-1])
+      min = Math.min( matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1])
     }
 
-    // console.log(min)
-    if (matrix[y][x-1] === min) {
-      // sequence.push({x: matrix[matrix.length-1][x-1], y: matrix[y][0]})
-      sequence.push({x: x-1, y: matrix.length-1 - y})
-      x--
+    if (i === 0) {
+      bestPath.push( {s1: i, s2: j-1} )
+      j--
     }
-    else if (matrix[y+1][x] === min) {
-      // sequence.push({x: matrix[matrix.length-1][x], y: matrix[y+1][0]})
-      sequence.push({x: x, y: matrix.length-1 - y-1})
-
-      y++
+    else if (j === 0) {
+      bestPath.push( {s1: i-1, s2: j} )
+      i--
     }
     else {
-      // sequence.push({x: matrix[matrix.length-1][x-1], y: matrix[y+1][0]})
-      sequence.push({x: x-1, y: matrix.length-1 - y-1})
-
-      x--
-      y++
+      bestPath.push( {s1: i-1, s2: j-1} )
+      i--
+      j--
     }
   }
-
-  sequence[sequence.length-1] = {x: matrix[matrix.length-1][1], y: matrix[matrix.length-2][0]} // gestion fin dans l'angle
-  sequence[sequence.length-1] = {x: 1, y: 1} // gestion fin dans l'angle
-  console.log(sequence)
-  return sequence.reverse()
+  return bestPath.reverse()
 }
 
-
+// let s1 = [1,4,8,3,3,1,4]
+// let s2 = [3,7,3,4,0]
+// let matrix = computeMatrix(s1, s2)
+// getOptimalPath(matrix, s1, s2)
 
 let sequence1 = []
 let sequence2 = []
 let drawingS1 = true
 let drawingS2 = false
 
-let matrix = computeMatrix([3,7,3,4,0], [1,4,8,3,3,1,4])
-getOptimalPath(matrix)
-
 const startDraw = event => drawing = true
 const stopDraw = event => drawing = false
-let sum = 0
+let distanceSinceLastPoint = 0
 const draw = event => {
-  console.log(event.movementX)
-  sum += Math.abs(event.movementX) + Math.abs(event.movementY)
-  if (sum > 50 && drawing) {
+  distanceSinceLastPoint += Math.abs(event.movementX) + Math.abs(event.movementY)
+  if (distanceSinceLastPoint > distanceBetweenPoints && drawing) {
     mouse = {x: event.clientX, y: event.clientY}
     recordDrawing()
-    sum = 0
+    distanceSinceLastPoint = 0
   }
 
 }
@@ -125,11 +100,18 @@ const recordDrawing = () => {
   if (drawing){
     if (drawingS1) sequence1.push({x: mouse.x, y: mouse.y})
     else if (drawingS2) sequence2.push({x: mouse.x, y: mouse.y})
-    dessin()
+    drawSequences()
+    if (sequence1.length > 0 && sequence2.length > 0) {
+      let matrix = computeMatrix(sequence1, sequence2)
+      // console.table(matrix)
+      let opt = getOptimalPath(matrix, sequence1, sequence2)
+      console.log(opt)
+      drawOpt(opt, sequence1, sequence2)
+    }
   }
 }
 
-const dessin = () => {
+const drawSequences = () => {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   ctx.strokeStyle = "#000000"
@@ -150,21 +132,10 @@ const dessin = () => {
 
 const drawOpt = (opt, sequence1, sequence2) => {
   ctx.strokeStyle = "#00ff00"
-  console.log("oui")
-  console.log(opt)
   for (let i = 0; i < opt.length; i++) {
-    console.log("i", i)
     ctx.beginPath()
-    // console.log(sequence2[opt[i].y-1] * 10)
-    // ctx.moveTo(sequence2[opt[i].x-1]*i * 10 + 100, 500 - sequence2[opt[i].x-1] * 10 + 100) // s1
-    // ctx.lineTo(sequence1[opt[i].y-1] *i* 10 + 100, 500 - sequence1[opt[i].y-1] * 10 + 100)
-    console.log(sequence2)
-    console.log(sequence2[0])
-    console.log(opt)
-    console.log(opt)
-    ctx.moveTo(sequence2[opt[i].x-1].x, sequence2[opt[i].x-1].y) // s1
-    ctx.lineTo(sequence1[opt[i].y-1].x, sequence1[opt[i].y-1].y)
-
+    ctx.moveTo(sequence1[opt[i].s1].x, sequence1[opt[i].s1].y)
+    ctx.lineTo(sequence2[opt[i].s2].x, sequence2[opt[i].s2].y)
     ctx.stroke()
   }
 }
@@ -178,49 +149,5 @@ document.addEventListener("keypress", key => {
     drawingS1 = false
     drawingS2 = true
   }
-  if (key.key === "e") {
-    // let matrix = computeMatrix([3,7,3,4,0], [1,4,8,3,3,1,4])
-    let matrix = computeMatrix(sequence1, sequence2)
-    let opt = getOptimalPath(matrix)
-    drawOpt(opt, sequence1, sequence2)
-  }
-
-  if (key.key === "r") {
-    let matrix = computeMatrix([3,7,3,4,0], [1,4,8,3,3,1,4])
-    let opt = getOptimalPath(matrix)
-    drawOpt(opt, [3,7,3,4,0], [1,4,8,3,3,1,4])
-  }
-  console.log(key.key)
 })
 window.addEventListener('load', initCanvas)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const drawOpt = (opt, sequence1, sequence2) => {
-//   ctx.strokeStyle = "#00ff00"
-//
-//   for (let i = 0; i < opt.length; i++) {
-//     console.log("i", i)
-//     ctx.beginPath()
-//     // console.log(sequence2[opt[i].y-1] * 10)
-//     // ctx.moveTo(sequence2[opt[i].x-1]*i * 10 + 100, 500 - sequence2[opt[i].x-1] * 10 + 100) // s1
-//     // ctx.lineTo(sequence1[opt[i].y-1] *i* 10 + 100, 500 - sequence1[opt[i].y-1] * 10 + 100)
-//
-//     ctx.moveTo(opt[i].x * 10 + 100, 500 - sequence2[opt[i].x-1] * 10 + 100) // s1
-//     ctx.lineTo(opt[i].y * 10 + 100, 500 - sequence1[opt[i].y-1] * 10 + 100)
-//
-//     ctx.stroke()
-//   }
-// }
